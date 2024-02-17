95304c435edc579c11db88d18342f482ca7aa693
Juan Franco Ledesma <juanfledesma18@gmail.com>
2024-02-17 17:50:53 -0300

    Created person module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PersonModule } from './person/person.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://jledesma:B4jwjMUH9vTuC0cc@spartanserver.c8ydza9.mongodb.net/'),
+    PersonModule,
   ],
   controllers: [],
   providers: [],
